8f4c667c261e7ac6959f232a2954514f245ba11b
mahmoudgouda419 <74051871+mahmoudgouda419@users.noreply.github.com>
2026-05-23 19:28:26 +0300

    Add files via upload

--- js/theme.js
+++ js/theme.js
@@ -1,29 +1,31 @@
-// Check for saved theme or system preference
 const savedTheme = localStorage.getItem("theme");
-const prefersDark = window.matchMedia("(prefers-color-scheme: dark)").matches;
 
-if (savedTheme === "light" || (!savedTheme && !prefersDark)) {
-  document.documentElement.setAttribute("data-theme", "light");
+if (savedTheme) {
+  document.documentElement.setAttribute("data-theme", savedTheme);
 } else {
-  document.documentElement.setAttribute("data-theme", "dark");
+  const prefersDark = window.matchMedia("(prefers-color-scheme: dark)").matches;
+
+  document.documentElement.setAttribute(
+    "data-theme",
+    prefersDark ? "dark" : "light",
+  );
 }
 
-// Function triggered by the checkbox
 function toggleTheme() {
   const currentTheme = document.documentElement.getAttribute("data-theme");
+
   const newTheme = currentTheme === "light" ? "dark" : "light";
 
   document.documentElement.setAttribute("data-theme", newTheme);
+
   localStorage.setItem("theme", newTheme);
 }
 
-// Ensure the switch is in the correct position when the page loads
 document.addEventListener("DOMContentLoaded", () => {
-  setTimeout(() => {
-    const themeCheckbox = document.getElementById("theme-checkbox");
-    if (themeCheckbox) {
-      themeCheckbox.checked =
-        document.documentElement.getAttribute("data-theme") === "light";
-    }
-  }, 100); // Slight delay to ensure the navbar component has injected
+  const themeCheckbox = document.getElementById("theme-checkbox");
+
+  if (themeCheckbox) {
+    themeCheckbox.checked =
+      document.documentElement.getAttribute("data-theme") === "light";
+  }
 });
